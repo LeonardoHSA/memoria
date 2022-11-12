1c63cd0a2460c7da473f08520bf848169e86d6c2
Leonardo <leohsanfins@hotmail.com>
2022-11-12 14:55:00 -0300

    Finalizando o projeto

--- memoria/lib/home_page.dart
+++ memoria/lib/home_page.dart
@@ -15,34 +15,37 @@ class HomePage extends StatelessWidget {
     return Scaffold(
         body: Padding(
       padding: const EdgeInsets.all(24.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const Logo(),
-          StartButton(
-              title: 'Modo Normal',
-              color: Colors.white,
-              action: () => Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (BuildContext context) =>
-                          const NivelPage(modo: Modo.normal),
-                    ),
-                  )),
-          StartButton(
-              title: 'Modo Insano',
-              color: MememoriaTheme.color,
-              action: () => Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (BuildContext context) =>
-                          const NivelPage(modo: Modo.insano),
-                    ),
-                  )),
-          const SizedBox(height: 60),
-          const Recordes(),
-        ],
+      child: SingleChildScrollView(
+        padding: const EdgeInsets.all(32),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Logo(),
+            StartButton(
+                title: 'Modo Normal',
+                color: Colors.white,
+                action: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (BuildContext context) =>
+                            const NivelPage(modo: Modo.normal),
+                      ),
+                    )),
+            StartButton(
+                title: 'Modo Insano',
+                color: MememoriaTheme.color,
+                action: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (BuildContext context) =>
+                            const NivelPage(modo: Modo.insano),
+                      ),
+                    )),
+            const SizedBox(height: 60),
+            const Recordes(),
+          ],
+        ),
       ),
     ));
   }
--- memoria/lib/widgets/logo.dart
+++ memoria/lib/widgets/logo.dart
@@ -1,19 +1,24 @@
+// ignore_for_file: prefer_typing_uninitialized_variables
+
 import 'package:flutter/material.dart';
 import 'package:memoria/theme.dart';
+import 'package:memoria/responsive/app_responsive_ext.dart';
 
 class Logo extends StatelessWidget {
   const Logo({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    double? w;
+    double? h;
     return Column(
       children: [
         Padding(
           padding: const EdgeInsets.only(bottom: 30),
           child: Image.asset(
             'images/logo.jpg',
-            width: 100,
-            height: 150,
+            width: w?.wsp,
+            height: h?.hsp,
           ),
         ),
         Padding(
